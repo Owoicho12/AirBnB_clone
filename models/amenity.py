56a4bec44d78f9e820f14c816830ce546ec8431a
Joshua Owoichoche Amah <115462208+Owoicho12@users.noreply.github.com>
2023-05-12 17:05:14 +0100

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Definition of the Amenity class. """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ This class represent an Amenity and inherits from the BaseModel. """
+    name = ""
